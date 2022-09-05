3608d545be2ad127ed59a720313a804330d845f5
fanpeichao <fan552426811@outlook.com>
2022-09-05 18:00:35 +0800

    add all

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,37 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true,
-		"node": true
-	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-	],
-	"overrides": [
-	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"plugins": [
-		"@typescript-eslint",
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"windows"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		],
-	}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    // indent: ["error", "tab"],
+    // "linebreak-style": ["error", "windows"],
+    // quotes: ["error", "double"],
+    // semi: ["error", "always"],
+    "prettier/prettier": [
+      "warn",
+      {
+        singleQuote: false,
+        tabs: false,
+        parser: "flow",
+        endOfLine: "auto",
+        trailingComma: "es5",
+        proseWrap: "always",
+      },
+    ],
+  },
 };
--- cli.ts
+++ cli.ts
@@ -1,3 +1,6 @@
 import { Command } from "commander";
 const program = new Command();
-program.name("吃啥").description("饭点吃啥是一个世界性难题，这个命令行工具用来解决这个世界性难题").version("0.0.1");
+program
+  .name("吃啥")
+  .description("饭点吃啥是一个世界性难题，这个命令行工具用来解决这个世界性难题")
+  .version("0.0.1");
