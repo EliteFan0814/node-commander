d392423d3c0742da0487363cda13920014fbaed2
fanpeichao <fan552426811@outlook.com>
2022-09-07 13:36:35 +0800

    add inquirer

--- cli.ts
+++ cli.ts
@@ -1,30 +1,21 @@
 import { Command } from "commander";
-import { add } from "./index";
+import api from "./index";
 const program = new Command();
 program.name("任务清单").description("一个简单的任务清单。").version("0.0.1");
 program.option("-l,--list", "列出所有任务");
 
 program
-  .command("clone <source> [destination]")
-  .description("clone a repository into a newly created directory")
-  .action((source, destination) => {
-    console.log(source);
-    console.log(destination);
-  });
-
-program
   .command("clear")
-  .description("clear all tasks")
+  .description("清空所有任务")
   .action((source, destination) => {
-    console.log(source);
+    api.clear();
   });
 program
-  .command("add <source...>")
+  .command("add <source>")
   .description("添加一个任务")
   .action((source, destination) => {
-    console.log(source);
-    console.log(destination);
-
-    add(source);
+    const options = program.opts();
+    api.add(source);
   });
+console.log(111, process.argv);
 program.parse(process.argv);
--- index.ts
+++ index.ts
@@ -3,8 +3,35 @@ import path from "path";
 import fs from "fs";
 const dbPath = path.join(homedir(), ".todo");
 
-export const read = (path = dbPath) => {
-  return new Promise((resolve, reject) => {
+export const add = async (title: string) => {
+  try {
+    const list = await read(dbPath);
+    list.push({ title, done: false });
+    await write(list, dbPath);
+  } catch (err) {
+    console.log("添加失败");
+  }
+};
+export const clear = async () => {
+  try {
+    await reset(dbPath);
+  } catch (err) {
+    console.log("清空失败！");
+  }
+};
+// 重置文件
+const reset = (path = dbPath) => {
+  return new Promise<void>((resolve, reject) => {
+    fs.writeFile(path, "[]", (err) => {
+      if (err) return reject(err);
+      resolve();
+      console.log("清空成功！");
+    });
+  });
+};
+// 读取文件
+const read = (path = dbPath) => {
+  return new Promise<object[]>((resolve, reject) => {
     fs.readFile(path, { flag: "a+" }, (error, data) => {
       if (error) {
         reject(error);
@@ -20,24 +47,20 @@ export const read = (path = dbPath) => {
     });
   });
 };
-export const add = (title: string) => {
-  fs.readFile(dbPath, { flag: "a+" }, (err, data) => {
-    if (err) throw err;
-    let list;
-    try {
-      list = JSON.parse(data.toString());
-    } catch (dataErr) {
-      list = [];
-    }
-    const task = {
-      title,
-      done: false,
-    };
-    list.push(task);
+// 写入文件
+const write = (list: object[], path = dbPath) => {
+  return new Promise<void>((resolve, reject) => {
     const string = JSON.stringify(list);
-    fs.writeFile(dbPath, string + "\n", (err) => {
-      if (err) throw err;
-      console.log("The file has been saved!");
+    fs.writeFile(path, string + "\n", async (err) => {
+      if (err) return reject(err);
+      resolve();
+      console.log("写入成功！");
+      const list = await read(dbPath);
+      console.log(list);
     });
   });
+};
+export default {
+  add,
+  clear,
 };
